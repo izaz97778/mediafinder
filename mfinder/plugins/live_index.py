d0220b6e08f94cf293f0dc08b9b1d95436d66ddd
izaz97778 <izaz97778@gmail.com>
2025-09-01 15:53:51 +0500

    Update live_index.py

--- mfinder/plugins/live_index.py
+++ mfinder/plugins/live_index.py
@@ -2,6 +2,7 @@ from pyrogram import Client, filters
 from mfinder import DB_CHANNELS, LOGGER
 from mfinder.db.files_sql import save_file
 from mfinder.utils.helpers import edit_caption
+from mfinder.plugins.start_cmd import START_INDEX_AFTER  # import the global variable
 
 media_filter = filters.document | filters.video | filters.audio
 
@@ -9,15 +10,18 @@ media_filter = filters.document | filters.video | filters.audio
 @Client.on_message(filters.chat(DB_CHANNELS) & media_filter)
 async def live_index(bot, message):
     try:
+        # Skip messages before START_INDEX_AFTER
+        if message.message_id <= START_INDEX_AFTER:
+            return
+
         for file_type in ("document", "video", "audio"):
             media = getattr(message, file_type, None)
-
             if not media:
-                break
+                continue  # changed from break to continue to check other types
+
             file_name = media.file_name
             file_name = edit_caption(file_name)
             media.file_type = file_type
-            # media.caption = message.caption if message.caption else file_name
             media.caption = file_name
             await save_file(media)
 
